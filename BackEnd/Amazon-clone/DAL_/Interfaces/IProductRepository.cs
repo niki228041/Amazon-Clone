c0646ce54583714452fd1ad4947c033050bbcc41
pavlo999 <80537100+pavlo999@users.noreply.github.com>
2023-05-23 21:21:00 +0300

    Adding structure

---
+++ BackEnd/Amazon-clone/DAL_/Interfaces/IProductRepository.cs
@@ -0,0 +1,20 @@
+﻿using DAL_.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL_.Interfaces
+{
+    public interface IProductRepository : IGenericRepository<ProductEntity, int>
+    {
+        IQueryable<ProductEntity> Products { get; }
+
+        Task<IQueryable<ProductEntity>> SearchProducts(string name);
+        Task<ProductEntity> GetByName(string name);
+        ICollection<ProductEntity> GetProductsAsync(GetProductsVM model);
+
+
+    }
+}
