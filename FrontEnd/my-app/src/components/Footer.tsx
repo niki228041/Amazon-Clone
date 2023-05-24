a1e5fc9d857c6329a8876f878bbecfc9bc185586
niki228041 <lopocznikita@gmail.com>
2023-05-24 23:25:11 +0200

    added started page

---
+++ FrontEnd/my-app/src/components/Footer.tsx
@@ -0,0 +1,13 @@
+
+const Footer=()=> {
+    return <>
+    <div className="bg-black w-full h-20 flex justify-center">
+        <p className=" text-white text-center ">
+            Footer
+        </p>
+    </div>
+    </>;
+  }
+  
+  export default Footer;
+  
---
+++ FrontEnd/my-app/src/components/Header.tsx
@@ -0,0 +1,14 @@
+import "../index.css"
+
+const Header=()=> {
+    return <>
+    <div className="bg-black w-full h-6">
+        <p className=" text-white text-center ">
+            Header
+        </p>
+    </div>
+    </>;
+  }
+  
+  export default Header;
+  
