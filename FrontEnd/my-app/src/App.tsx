9dcfdaa812e3a1f0fc6a8ed25933b833e1285e4c
Timur Ulanovsky <ulanovskytimur@gmail.com>
2023-07-22 23:17:33 +0300

    Sends a letter to gmail.

--- FrontEnd/my-app/src/App.tsx
+++ FrontEnd/my-app/src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { Route,Router,Routes,BrowserRouter,Outlet, Link} from 'react-router-dom';
+// import { BrowserRouter as Router, Route, Routes, Outlet } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Main from './components/Main';
@@ -14,64 +14,69 @@ import LoginScreen from './components/Auth/Login';
 import { useDispatch, useSelector } from 'react-redux';
 import { GetAccessToken } from './api/jwtDecodeToken';
 import { AuthUser } from './features/user/user-slice';
+import ForgotPasswordScreen from './components/Auth/Forgot-Password';
+import ResetPasswordScreen from './components/Auth/Reset-Password';
+import { BrowserRouter, Outlet, Route, Routes } from 'react-router-dom';
 
-const App:React.FC =()=> {
+const App: React.FC = () => {
   var dispatch = useDispatch();
   const token = GetAccessToken();
 
-  var isAuth = useSelector((state:any)=>state.user.isAuth);
+  var isAuth = useSelector((state: any) => state.user.isAuth);
 
 
-  useEffect(()=>{
-      if(token){
-          dispatch(AuthUser(token));
-      }
-  },[])
-  
+  useEffect(() => {
+    if (token) {
+      dispatch(AuthUser(token));
+    }
+  }, [])
+
   return (
     <BrowserRouter>
-        <Routes>
-          <Route
-            path='/'
-            element={
-              <>
-                <div className="flex flex-col" style={{ minHeight: "180vh" }}>
-                  <Header />
-                    <Outlet />
+      <Routes>
+        <Route
+          path='/'
+          element={
+            <>
+              <div className="flex flex-col" style={{ minHeight: "180vh" }}>
+                <Header />
+                <Outlet />
                 <div className='mt-auto'>
 
                   <Footer />
                 </div>
 
-                </div>
-              </>
-            }
-          >
+              </div>
+            </>
+          }
+        >
 
 
-            <Route path='admin' element={<Outlet/>}>
-              <Route path='' element={<><AdminSite/></>}/>
-              <Route path='create'>
-                <Route path='products' element={<CreateProduct/>} />
-                <Route path='categories' element={<CreateCategory/>} />
-              </Route>
+          <Route path='admin' element={<Outlet />}>
+            <Route path='' element={<><AdminSite /></>} />
+            <Route path='create'>
+              <Route path='products' element={<CreateProduct />} />
+              <Route path='categories' element={<CreateCategory />} />
             </Route>
-
-          
-            <Route path="/products" element={<><Main /></>} >
-              <Route path="products" element={<Profile />} />
-            </Route>
-
-            <Route path='/player' element={<Player/>}>
-
-            </Route>
-
-            <Route path='orders' element={<Orders/>}/>
-            <Route path="product/:productId" element={<OneProduct />} />
-            <Route path="profile" element={<Profile />} />
-            <Route path="login" element={<LoginScreen />} />
           </Route>
-        </Routes>
+
+
+          <Route path="/products" element={<><Main /></>} >
+            <Route path="products" element={<Profile />} />
+          </Route>
+
+          <Route path='/player' element={<Player />}>
+
+          </Route>
+
+          <Route path='orders' element={<Orders />} />
+          <Route path="product/:productId" element={<OneProduct />} />
+          <Route path="profile" element={<Profile />} />
+          <Route path="login" element={<LoginScreen />} />
+          <Route path="forgotpassword" element={<ForgotPasswordScreen />} />
+          <Route path="resetpassword/:userId" element={<ResetPasswordScreen />} />
+        </Route>
+      </Routes>
     </BrowserRouter>
 
   );
