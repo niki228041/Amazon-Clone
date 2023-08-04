8491ab406273319c74f1974388cdc37f7e2d3fcb
niki228041 <lopocznikita@gmail.com>
2023-08-04 12:21:09 +0200

    fix

--- FrontEnd/my-app/src/App.tsx
+++ FrontEnd/my-app/src/App.tsx
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
-import React from 'react';
-import { Route, Router, Routes, BrowserRouter, Outlet, Link } from 'react-router-dom';
-=======
 import React, { useEffect } from 'react';
 // import { BrowserRouter as Router, Route, Routes, Outlet } from 'react-router-dom';
->>>>>>> 95b6fedb1947089a23f5ca28ad7a51acf9e8298a
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Profile from './components/Profile/profile';
@@ -21,29 +16,6 @@ import Payment from './components/Profile/Payment';
 import Address from './components/Profile/Address';
 import Order from './components/Profile/Order';
 import EditProfile from './components/Profile/EditProfile';
-<<<<<<< HEAD
-import AdminHeader from './components/Admin_Page/admin-header';
-import AdminSidebar from './components/Admin_Page/admin-sidebar';
-import AdminHomePage from './components/Admin_Page/adminhomepage';
-import UsersTable from './components/Admin_Page/users-table';
-import ShopsTable from './components/Admin_Page/shops-table';
-import ProductsTable from './components/Admin_Page/products-table';
-import OrdersTable from './components/Admin_Page/orders-table';
-import CustomersTable from './components/Admin_Page/customers-table';
-
-function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/admin'
-          element={<>
-            <div className="flex flex-col" style={{ minHeight: "120vh", background: "rgb(231, 238, 240)" }}>
-              <AdminHeader />
-              <AdminSidebar></AdminSidebar>
-              <div style={{ flex: "1" }}>
-                <Outlet />
-              </div>
-=======
 import Player from './components/Player/Player';
 import LoginScreen from './components/Auth/Login';
 import { useDispatch, useSelector } from 'react-redux';
@@ -65,6 +37,15 @@ import OrdersList from './components/BuyProduct/OrdersList';
 import SuccessfulPurchase from './components/BuyProduct/SuccessfulPurchase';
 
 
+import AdminHeader from './components/Admin_Page/admin-header';
+import AdminSidebar from './components/Admin_Page/admin-sidebar';
+import AdminHomePage from './components/Admin_Page/adminhomepage';
+import UsersTable from './components/Admin_Page/users-table';
+import ShopsTable from './components/Admin_Page/shops-table';
+import ProductsTable from './components/Admin_Page/products-table';
+import OrdersTable from './components/Admin_Page/orders-table';
+import CustomersTable from './components/Admin_Page/customers-table';
+
 const App: React.FC = () => {
   var dispatch = useDispatch();
   const token = GetAccessToken();
@@ -81,11 +62,31 @@ const App: React.FC = () => {
   return (
     <BrowserRouter>
       <Routes>
-            <Route path='/music' element={<><MusicHeader/><div className="flex flex-col bg-black" style={{ minHeight: "100vh" }}><Outlet/></div></>} >
-              <Route path='' element={<Player/>} />
-              <Route path='createGenre' element={<CreateGenre/>} />
-              <Route path='createTrack' element={<CreateTrack/>} />
-            </Route>
+          <Route path='/music' element={<><MusicHeader/><div className="flex flex-col bg-black" style={{ minHeight: "100vh" }}><Outlet/></div></>} >
+            <Route path='' element={<Player/>} />
+            <Route path='createGenre' element={<CreateGenre/>} />
+            <Route path='createTrack' element={<CreateTrack/>} />
+          </Route>
+
+          <Route path='/admin'
+          element={
+          <>
+            <div className="flex flex-col" style={{ minHeight: "100vh", background: "rgb(231, 238, 240)" }}>
+              <AdminHeader />
+              <AdminSidebar></AdminSidebar>
+                <Outlet />
+            </div> 
+          </>}>
+              <Route path='userstable' element={<UsersTable />} />
+          <Route path='shopstable' element={<ShopsTable />} />
+          <Route path='producttable' element={<ProductsTable />} />
+          <Route path='orderstable' element={<OrdersTable />} />
+          <Route path='customerstable' element={<CustomersTable />} />
+          <Route path='products' element={<CreateProduct />} />
+          <Route path='categories' element={<CreateCategory />} />
+          </Route>
+
+
           <Route
           path='/'
           element={
@@ -102,61 +103,22 @@ const App: React.FC = () => {
             </>
           }
         >
->>>>>>> 95b6fedb1947089a23f5ca28ad7a51acf9e8298a
-
-            </div>
-          </>}>
-          <Route path='userstable' element={<UsersTable />} />
-          <Route path='shopstable' element={<ShopsTable />} />
-          <Route path='producttable' element={<ProductsTable />} />
-          <Route path='orderstable' element={<OrdersTable />} />
-          <Route path='customerstable' element={<CustomersTable />} />
-          <Route path='products' element={<CreateProduct />} />
-          <Route path='categories' element={<CreateCategory />} />
-        </Route>
-
-<<<<<<< HEAD
-        <Route
-          path='/'
-          element={<>
-            <div className="flex flex-col" style={{ minHeight: "120vh" }}>
-              <Header />
-              <div style={{ flex: "1" }}>
-                <Outlet />
-              </div>
-              <Footer />
-            </div>
-          </>}
-        >
 
 
-          {/* <Route path='/admin' element={<Outlet />}>
-              <Route path='' element={<><AdminSite /></>} />
-=======
-            <Route path='admin' element={<Outlet/>}>
+            {/* <Route path='admin' element={<Outlet/>}>
                 <Route path='' element={<><AdminSite/></>}/>
->>>>>>> 95b6fedb1947089a23f5ca28ad7a51acf9e8298a
               <Route path='create'>
-                <Route path='products' element={<CreateProduct />} />
-                <Route path='categories' element={<CreateCategory />} />
+                <Route path='products' element={<CreateProduct/>} />
+                <Route path='categories' element={<CreateCategory/>} />
               </Route>
             </Route> */}
 
+          
+            {/* <Route path="/products" element={<><Main /></>} >
+              <Route path="products" element={<Profile />} />
+            </Route> */}
 
-<<<<<<< HEAD
-          {/* <Route path="/products" element={<><Main /></>} >
-                <Route path="products" element={<Profile />} />
-              </Route> */}
 
-          <Route path='orders' element={<Orders />} />
-          <Route path="product/:productId" element={<OneProduct />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="login" element={<LoginScreen />} />
-          <Route path="/payment" element={<Payment />} />
-          <Route path="/address" element={<Address />} />
-          <Route path="/proforder" element={<Order />} />
-          <Route path="/editprofile" element={<EditProfile />} />
-=======
             <Route path='orders' element={<Orders/>}/>
             <Route path='successful-purchase' element={<SuccessfulPurchase/>} />
 
@@ -177,8 +139,7 @@ const App: React.FC = () => {
             </Route>
 
             
-
-          
+            
       
           <Route path="/products" element={<><Main /></>} >
             <Route path="products" element={<Profile />} />
@@ -196,11 +157,9 @@ const App: React.FC = () => {
           <Route path="forgotpassword" element={<ForgotPasswordScreen />} />
           {/* <Route path="resetpassword/:userId" element={<ResetPasswordScreen />} /> */}
           <Route path="resetpassword/" element={<ResetPasswordScreen />} />
->>>>>>> 95b6fedb1947089a23f5ca28ad7a51acf9e8298a
         </Route>
       </Routes>
     </BrowserRouter>
-
 
   );
 }
