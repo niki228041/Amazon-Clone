b799579f1e7dce349d313f73369e22a59c394bfa
niki228041 <lopocznikita@gmail.com>
2023-09-09 13:08:38 +0200

    edit fuctionality

--- BackEnd/Amazon-clone/DAL/Interfaces/IProductRepository.cs
+++ BackEnd/Amazon-clone/DAL/Interfaces/IProductRepository.cs
@@ -1,5 +1,7 @@
 ﻿using DAL.Entities;
+using DAL.Entities.FilterEntities;
 using Infrastructure.Models;
+using Microsoft.EntityFrameworkCore;
 
 
 namespace DAL.Interfaces;
@@ -11,6 +13,13 @@ public interface IProductRepository : IGenericRepository<Product, int>
     Task<Product> GetByName(string name);
     ICollection<Product> GetProductsAsync(GetProductsVM model);
     ICollection<Product> GetProductsAsync();
+
+    public Task RemoveVariantProductsAsync(int productId);
+    public Task RemoveProductImagesAsync(int productId);
+
+    public Task AddVariantProductsToProductAsync(int productId, List<int> variantsIds);
+
+    public Task SaveChangesAsync();
 
 }
 
